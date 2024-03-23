43e346d8d2436a2ef2e92e0d8e3a32afaa7fd92e
kun-kin <nguyenquyenlinhnb@gmail.com>
2024-03-23 09:19:27 +0700

    up
    up1

--- assets/js/slide.js
+++ assets/js/slide.js
@@ -38,7 +38,35 @@ var swiper = new Swiper(".related-post-list", {
         },
     },
 });
-
+var swiper = new Swiper(".related-blogs-slider", {
+    slidesPerView: 3,
+    spaceBetween: 35,
+    loop: true,
+    autoplay: false,
+    speed: 600,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        1366: {
+            slidesPerView: 3,
+            spaceBetween: 35,
+        },
+        1024: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        374: {
+            slidesPerView: 1,
+            spaceBetween: 15,
+        },
+    },
+});
 var swiper = new Swiper(".testimonials-slide", {
     slidesPerView: 1,
     loop: true,
@@ -76,6 +104,72 @@ var swiper = new Swiper(".blogs-slide", {
         374: {
             slidesPerView: 1,
             spaceBetween: 15,
+        },
+    },
+});
+
+
+var swiper = new Swiper(".partner-slide", {
+    slidesPerView: 6,
+    spaceBetween: 35,
+    loop: true,
+    autoplay: false,
+    speed: 600,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        1366: {
+            slidesPerView: 6,
+            spaceBetween: 35,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 15,
+        },
+        374: {
+            slidesPerView: 1,
+            spaceBetween: 15,
+        },
+    },
+});
+
+
+var swiper = new Swiper(".teams-slide", {
+    slidesPerView: 6,
+    spaceBetween: 0,
+    loop: true,
+    autoplay: false,
+    speed: 600,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    breakpoints: {
+        1366: {
+            slidesPerView: 4,
+            spaceBetween: 0,
+        },
+        1024: {
+            slidesPerView: 4,
+            spaceBetween: 0,
+        },
+        991: {
+            slidesPerView: 3,
+            spaceBetween: 0,
+        },
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 0,
+        },
+        374: {
+            slidesPerView: 1,
+            spaceBetween: 0,
         },
     },
 });
